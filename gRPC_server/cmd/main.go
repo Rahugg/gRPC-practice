431916f4bc079a726a8970a61af16cb021761e83
Rahugg <rahugg07@gmail.com>
2023-10-31 11:09:33 +0600

    grpc_server

---
+++ gRPC_server/cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"gRPC/gRPC_server/pb"
+)
+
+func main() {
+	addr := ":9292"
+
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("error: can't listen - %s", err)
+	}
+
+	srv := grpc.NewServer()
+	var u Rides
+	pb.RegisterRidesServer(srv, &u)
+	reflection.Register(srv)
+
+	log.Printf("info: server ready on %s", addr)
+	if err = srv.Serve(lis); err != nil {
+		log.Fatalf("error: can't serve - %s", err)
+	}
+}
+func (r *Rides) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartResponse, error) {
+	// TODO: Validate req
+	resp := pb.StartResponse{
+		Id: req.Id,
+	}
+
+	// TODO: Work (insert to database ...)
+	return &resp, nil
+}
+
+type Rides struct {
+	pb.UnimplementedRidesServer
+}
