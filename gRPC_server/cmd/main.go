b5d9d04ae40433cb9a0bbdccd4a9bb97b32740c5
Rahugg <rahugg07@gmail.com>
2023-10-31 12:19:04 +0600

    grpc gateway && e2e test && unit test

--- gRPC_server/cmd/main.go
+++ gRPC_server/cmd/main.go
@@ -26,29 +26,19 @@ func main() {
 		log.Fatalf("error: can't listen - %s", err)
 	}
 
+	srv := createServer()
+	log.Printf("info: server ready on %s", addr)
+	if err := srv.Serve(lis); err != nil {
+		log.Fatalf("error: can't serve - %s", err)
+	}
+}
+
+func createServer() *grpc.Server {
 	srv := grpc.NewServer(grpc.UnaryInterceptor(timingInterceptor))
 	var u Rides
 	pb.RegisterRidesServer(srv, &u)
 	reflection.Register(srv)
-
-	log.Printf("info: server ready on %s", addr)
-	if err = srv.Serve(lis); err != nil {
-		log.Fatalf("error: can't serve - %s", err)
-	}
-}
-func (r *Rides) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartResponse, error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, status.Errorf(codes.Unauthenticated, "no metadata")
-	}
-	log.Printf("info:key is %s", md["key"])
-	// TODO: Validate req
-	resp := pb.StartResponse{
-		Id: req.Id,
-	}
-
-	// TODO: Work (insert to database ...)
-	return &resp, nil
+	return srv
 }
 
 func timingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
@@ -60,29 +50,54 @@ func timingInterceptor(ctx context.Context, req interface{}, info *grpc.UnarySer
 
 	return handler(ctx, req)
 }
+
 func (r *Rides) Location(stream pb.Rides_LocationServer) error {
 	count := int64(0)
 	driverID := ""
+
 	for {
 		req, err := stream.Recv()
 		if errors.Is(err, io.EOF) {
 			break
 		}
 		if err != nil {
-			err = status.Errorf(codes.Internal, "can't read")
-			if err != nil {
-				return err
-			}
+			return status.Errorf(codes.Internal, "can't read")
 		}
-		// TODO: update db
+		// TODO: Update DB
 		driverID = req.DriverId
 		count++
 	}
+
 	resp := pb.LocationResponse{
 		DriverId: driverID,
 		Count:    count,
 	}
 	return stream.SendAndClose(&resp)
+}
+
+func (r *Rides) End(ctx context.Context, req *pb.EndRequest) (*pb.EndResponse, error) {
+	// TODO: Validate req
+	resp := pb.EndResponse{
+		Id: req.Id,
+	}
+
+	// TODO: Work (insert to database ...)
+	return &resp, nil
+}
+
+func (r *Rides) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartResponse, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Errorf(codes.Unauthenticated, "no metadata")
+	}
+	log.Printf("info: api_key %s", md["api_key"])
+	// TODO: Validate req
+	resp := pb.StartResponse{
+		Id: req.Id,
+	}
+
+	// TODO: Work (insert to database ...)
+	return &resp, nil
 }
 
 type Rides struct {
