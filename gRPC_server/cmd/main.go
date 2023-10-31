f2004b45af89a177e901d45d572d97193b423d01
Rahugg <rahugg07@gmail.com>
2023-10-31 11:43:01 +0600

    grpc streaming

--- gRPC_server/cmd/main.go
+++ gRPC_server/cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
+	"io"
 	"log"
 	"net"
 
@@ -46,6 +48,31 @@ func (r *Rides) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartRespo
 
 	// TODO: Work (insert to database ...)
 	return &resp, nil
+}
+
+func (r *Rides) Location(stream pb.Rides_LocationServer) error {
+	count := int64(0)
+	driverID := ""
+	for {
+		req, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			err = status.Errorf(codes.Internal, "can't read")
+			if err != nil {
+				return err
+			}
+		}
+		// TODO: update db
+		driverID = req.DriverId
+		count++
+	}
+	resp := pb.LocationResponse{
+		DriverId: driverID,
+		Count:    count,
+	}
+	return stream.SendAndClose(&resp)
 }
 
 type Rides struct {
